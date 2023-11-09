2a62e6da97f684fb06631df679cfc54591c8dc93
AppSeed Generated Projects <133564452+appseed-projects6@users.noreply.github.com>
2023-11-09 16:11:12 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Customer(models.Model):
+
+    #__Customer_FIELDS__
+    firstname = models.CharField(max_length=255, null=True, blank=True)
+    lastname = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Customer_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Customer")
+        verbose_name_plural = _("Customer")
+
+
+
+#__MODELS__END
